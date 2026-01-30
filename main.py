fe76e0aea8a3f50080fcd515dfe38c486d6b501a
zpll <92767712+zpll@users.noreply.github.com>
2026-01-30 11:34:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ class MyPlugin(Star):
         message_str = event.message_str # 用户发的纯文本消息字符串
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
-        yield event.image_result("https://example.com/image.jpg") # 发送 URL 图片，务必以 http 或 https 开头
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        yield event.image_result("http://v9.img.360kuai.com/video/360_202_/t11508c75c8d3683d207fd5da5d.jpg?size=576x1024") # 发送 URL 图片，务必以 http 或 https 开头
+        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!!!!!!!") # 发送一条纯文本消息
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
