6a19b3da611e89dbe66baa868cb032a5df4bfc2a
zpll <92767712+zpll@users.noreply.github.com>
2026-01-30 11:43:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class MyPlugin(Star):
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!!!!") # 发送一条纯文本消息
      # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("图片")
-    async def helloworld(self, event: AstrMessageEvent):
+    async def photo(self, event: AstrMessageEvent):
         """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
         message_str = event.message_str # 用户发的纯文本消息字符串
