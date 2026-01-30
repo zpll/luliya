d770227159226cf7e40f6b7c1ca863a1ac973419
zpll <92767712+zpll@users.noreply.github.com>
2026-01-30 11:32:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class MyPlugin(Star):
         message_str = event.message_str # 用户发的纯文本消息字符串
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
+        yield event.image_result("https://example.com/image.jpg") # 发送 URL 图片，务必以 http 或 https 开头
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
 
     async def terminate(self):
