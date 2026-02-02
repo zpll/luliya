a96c131c6c5c4316808ea810b7193f0df138e556
zpll <92767712+zpll@users.noreply.github.com>
2026-02-02 08:27:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class MyPlugin(Star):
         message_str = event.message_str # 用户发的纯文本消息字符串
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
-        wupin= message_str.split(' ', 1)[1] if ' ' in text else ""
+        wupin= message_str.split(' ', 1)[1] if ' ' in message_str else ""
         url = 'https://api.x-metash.cn/h5/home/searchAppNew'
         headers = {
         "accept": "*/*",
